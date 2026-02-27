8c8894177a23b0f928b6901de5fa0d4c4634ee23
David Hope <david.hope@ec.gc.ca>
2026-02-27 14:22:44 -0500

    checks on Rstudio

--- inst/extdata/install_shiny_app.R
+++ inst/extdata/install_shiny_app.R
@@ -12,11 +12,15 @@ message("------ Installing R packages ------------")
 if (!requireNamespace("remotes")) {
   install.packages("remotes")
 }
+if (!requireNamespace("rstudioapi")) {
+  install.packages("rstudioapi")
+}
 remotes::install_github(
   "dhope/Shiny.CWS.ONT",
   dependencies = TRUE
 )
 file.copy(system.file("extdata/run_shiny_app.R",package = 'Shiny.CWS.ONT'), ".")
-rstudioapi::navigateToFile("run_shiny_app.R")
+if(Sys.getenv("RSTUDIO") == "1"){
+  rstudioapi::navigateToFile("run_shiny_app.R")}
 message("------ Done! ----------------------------")
 message("Source the 'run_shiny_app.R' file to update and run the app")
